f3ec4ea68233d8d26b666638ad7f7ace4f27798e
Nazeer <timeeversal@gmail.com>
2026-05-02 21:41:11 +0700

    Add files via upload

--- Web Company Sekolah La Libertad/asset/js/script.js
+++ Web Company Sekolah La Libertad/asset/js/script.js
@@ -37,6 +37,12 @@
 })();
 
 
+const toggle = document.getElementById('menu-toggle');
+const menu = document.querySelector('.nav-menu');
+
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
 /* ─────────────────────────────────────────
    HERO — staggered entrance
    ───────────────────────────────────────── */
